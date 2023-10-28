c22a726c8ece1c6c4ca047d89a5ef1b8c13e5411
Uktena-34 <dgomezamaton9@gmail.com>
2023-10-27 18:16:09 -0600

    Agregar archivos de proyecto.

---
+++ MediatorDesignPattern/UserAdmin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediatorDesignPattern
+{
+    public class UserAdmin : IColleague
+    {
+        public UserAdmin(IMediator mediator) : base(mediator)
+        {
+
+        }
+
+        public override void Receive(string message)
+        {
+            Console.WriteLine("Un administrador recibe: "+message);
+            Console.WriteLine("Se notifico por mail");
+        }
+    }
+}
